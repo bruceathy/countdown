bf78b414b23f54d0c48ef360f7e2f7be2160dd26
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-01-06 10:07:00 -0500

    timerchallenge component

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,8 @@
+export default function TimerChallenge() {
+  return (
+    <div className="challenge">
+      <h2>Timer</h2>
+      <p>Stop the timer once you estimate that time is (almost) up</p>
+    </div>
+  );
+}
